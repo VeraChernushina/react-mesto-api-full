3812555218d97412a0f8d777c5dc74c984300c19
Vera Chernushina <chernushina.vera@gmail.com>
2022-04-03 23:05:24 +0300

    fix: change logger

--- backend/middlewares/logger.js
+++ backend/middlewares/logger.js
@@ -2,7 +2,7 @@ const winston = require('winston');
 const expressWinston = require('express-winston');
 
 // логгер запросов
-const requestLogger = expressWinston.logger({
+const requestLogger = expressWinston.createLogger({
   transports: [
     new winston.transports.File({ filename: 'request.log' }),
   ],
