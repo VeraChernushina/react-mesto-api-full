f41ae9e48a36fa5cce33f8d91537661f2ecc0395
Vera Chernushina <chernushina.vera@gmail.com>
2022-05-15 18:46:28 +0300

    fix cors middleware

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -1,7 +1,8 @@
 const allowedCors = [
   'http://verachernushina.mesto.nomoredomains.work',
   'https://verachernushina.mesto.nomoredomains.work',
-  'localhost:3000'
+  'http://localhost:3000',
+  'https://localhost:3000'
 ]
 
 const DEFAULT_ALLOWED_METHODS = "GET,HEAD,PUT,PATCH,POST,DELETE";
